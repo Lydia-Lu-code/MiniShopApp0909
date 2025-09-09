945dc86775304966ea5ec229f5d41cd6a4d8ed6f
Lydia-Lu-code <lydialu8822@gmail.com>
2025-09-09 11:42:24 +0800

    Initial Commit

---
+++ MiniShopApp0909/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniShopApp0909
+//
+//  Created by Lydia Lu on 2025/9/9.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
